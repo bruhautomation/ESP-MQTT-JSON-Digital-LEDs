1c4d719aad338e9dcedbf23cd2668f0e261763b4
BRUH Automation <ben@bruhautomation.com>
2017-04-27 00:17:59 -0400

    Add dependency information

--- ESP_MQTT_Digital_LEDs/ESP_MQTT_Digital_LEDs.ino
+++ ESP_MQTT_Digital_LEDs/ESP_MQTT_Digital_LEDs.ino
@@ -7,7 +7,17 @@
   |______/  | _| `.__| \______/  |__|  |__|    /__/     \__\ \______/      |__|      \______/  |__|  |__| /__/     \__\  |__|     |__|  \______/  |__| \__|
 
   Thanks much to @corbanmailloux for providing a great framework for implementing flash/fade with HomeAssistant https://github.com/corbanmailloux/esp-mqtt-rgb-led
-
+  
+  To use this code you will need the following dependancies: 
+  
+  - Support for the ESP8266 boards. 
+        - You can add it to the board manager by going to File -> Preference and pasting http://arduino.esp8266.com/stable/package_esp8266com_index.json into the Additional Board Managers URL field.
+        - Next, download the ESP8266 dependancies by going to Tools -> Board -> Board Manager and searching for ESP8266 and installing it.
+  
+  - You will also need to download the follow libraries by going to Sketch -> Include Libraries -> Manage Libraries
+      - FastLED 
+      - PubSubClient
+      - ArduinoJSON
 */
 
 #define FASTLED_INTERRUPT_RETRY_COUNT 0 //makes animations smoother... supposedly... 
