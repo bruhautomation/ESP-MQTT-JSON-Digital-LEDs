157ce9f4f8a37a10095cc75af5ea6bb8a5a3922d
BRUH Automation <ben@bruhautomation.com>
2017-06-27 19:49:07 -0400

    Disable viewable WIFI network
    Thanks to willparker for pointing this out. This prevents the NodeMCU from creating a local WIFI hotspot.

--- ESP_MQTT_Digital_LEDs/ESP_MQTT_Digital_LEDs.ino
+++ ESP_MQTT_Digital_LEDs/ESP_MQTT_Digital_LEDs.ino
@@ -232,7 +232,8 @@ void setup_wifi() {
   Serial.println();
   Serial.print("Connecting to ");
   Serial.println(ssid);
-
+  
+  WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
 
   while (WiFi.status() != WL_CONNECTED) {
